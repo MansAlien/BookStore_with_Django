75cfb6580985350feeb0bdbe44a1ade1a6de6d36
MansAlien <mans012033945421@gmail.com>
2025-04-08 18:01:05 +0200

    create the viewset (list) of author and book

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Author.objects.all()
+        serializer = AuthorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class BookViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
